ab3aea72d01a914a2992ec560f5cfc7add1a8f1b
mushimiyimanaalice <mushimiyimanaalice274@gmail.com>
2026-05-28 06:56:44 +0200

    test url with cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://smarttax-ten.vercel.app'],
     credentials: true
   }
 });
